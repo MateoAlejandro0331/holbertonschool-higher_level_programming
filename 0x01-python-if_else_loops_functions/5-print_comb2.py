27d5c0a79ffca6a9a6946d54b8eebebbeaa05582
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-05-04 11:42:26 -0500

    Addinf task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range (0, 100):
-    if i < 10:
-        print(f"0{i}, ", end="")
-    elif i > 9 and i != 99:
-        print(f"{i}, ", end="")
+    if i < 99:
+        print(f"{str(i).zfill(2)}, ", end="")
     elif i == 99:
         print(f"{i}")
