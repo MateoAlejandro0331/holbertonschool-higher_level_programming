b29a4608cea388e54ec324e20fd1130fc67fb11a
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-05-05 16:28:44 -0500

    Adding task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     lens = len(argv)
     if lens == 1:
-        print(f"{len - 1} arguments.")
+        print("0 arguments.")
     else:
         print(f"{lens - 1} arguments:")
         for i in range(1, lens):
