87139a572df54beef280c67845dad513ac94bd0f
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-05-05 22:57:39 -0500

    Addinga advanced task1

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,12 +3,12 @@ from sys import argv
 from calculator_1 import add, sub, mul, div
 if __name__ == "__main__":
     lens = len(argv)
-    op = argv[2]
-    a = int(argv[1])
-    b = int(argv[3])
     if lens != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    op = argv[2]
+    a = int(argv[1])
+    b = int(argv[3])
     if op != "+" and op != "-" and op != "*" and op != "/":
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
