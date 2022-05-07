2d27f7e25330b4ec62007b5f308d9fe7b49251ca
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-05-06 19:00:09 -0500

    Adding task8

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    upper = ""
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            upper += chr(ord(i) - 32)
+        elif ord(i) >= 65 and ord(i) <= 90:
+            upper += chr(ord(i))
+        else:
+            upper += chr(ord(i))
+    print(upper)
