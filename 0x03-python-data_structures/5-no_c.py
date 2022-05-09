e0e0c60c78779662653d352878d988dfa379aa39
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-05-09 15:58:19 -0500

    Adding task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     if my_string:
-        new = my_string.replace("c", "")
-        new = new.replace("C", "")
+        new = ""
+        for i in range(len(my_string) - 1)
+            if i != "c" and i != "C":
+                new += i
         return(new)
