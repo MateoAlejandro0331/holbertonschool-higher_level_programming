d112726b5f81be44dba2810990f0df970105dea8
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-05-09 14:47:45 -0500

    adding changes to task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     lens = len(my_list) - 1
-    if idx < 0:
+    if idx < 0 or idx > lens:
         return(None)
-    if idx > lens:
-        return(None)
-    for i in range(lens):
-        if i == idx:
-            return(my_list[i])
+    else:
+        return(my_list[idx])
