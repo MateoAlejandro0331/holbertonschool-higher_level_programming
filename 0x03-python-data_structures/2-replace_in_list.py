3112bef8df596a8038dc589a0f4cb1aa1e87d308
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-05-09 14:49:18 -0500

    Adding changes to task 2

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,7 +5,5 @@ def replace_in_list(my_list, idx, element):
         return(my_list)
     if idx > lens:
         return(my_list)
-    for i in range(lens):
-        if i == idx:
-            my_list[i] = element
-            return(my_list)
+    my_list[idx] = element
+    return(my_list)
