0d658747d47e82de91f5f31418ec777034ddad98
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-05-09 19:21:34 -0500

    Adding task 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -7,6 +7,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
         lista.append(0)
     if len(listb) < 2:
         listb.append(0)
+    if len(lista) == 0:
+        lista.append(0)
+        lista.append(0)
+    if len(listb) == 0:
+        listb.append(0)
+        list.append(0)
     aux = lista[0] + listb[0]
     newlist.append(aux)
     aux = lista[1] + listb[1]
