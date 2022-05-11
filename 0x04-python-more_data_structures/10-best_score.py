9a4b4a0a4d80265f57a96a0ef7c4fdee91068098
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-05-10 20:28:44 -0500

    adding task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-            newlist = list(a_dictionary)
+            newlist = a_dictionary.values()
             max_value = max(newlist)
-            return max_value
+            for key, value in a_dictionary.items():
+                if value == max_value:
+                    return key
     else:
         return None
