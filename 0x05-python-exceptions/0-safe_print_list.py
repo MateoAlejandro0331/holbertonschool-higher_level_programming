94fd95c7d4d784eb61bb9ec730b3a876f0dd4596
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-05-23 12:36:28 -0500

    Adding task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        for idx, i in enumerate(my_list[:x+1]):
+            print(i, end="")
+        print()
+        return idx + 1
+    except IndexError:
+        return idx + 1
