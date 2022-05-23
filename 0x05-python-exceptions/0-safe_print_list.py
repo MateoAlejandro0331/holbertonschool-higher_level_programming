f92010e1606241006d99626186d0e209943ee3f6
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-05-23 12:38:32 -0500

    Adding task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        for idx, i in enumerate(my_list[:x+1]):
+        for idx, i in enumerate(my_list[:x]):
             print(i, end="")
         print()
         return idx + 1
