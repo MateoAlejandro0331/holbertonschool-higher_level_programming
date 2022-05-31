1141601c4a0b16c65b18b43cf47f0b28a3375aab
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-05-31 18:43:54 -0500

    Adding task 5

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+
+class Rectangle:
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __str__(self):
+        string = ""
+        if self.__height == 0 or self.__width == 0:
+            return string
+        for i in range(self.__height):
+            string += "#" * self.__width
+            if i < self.__height - 1:
+                string += "\n"
+        return string
+
+    def __del__(self):
+        print("Bye rectangle...")
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("heigth must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        else:
+            return 2 * self.__width + 2 * self.__height
