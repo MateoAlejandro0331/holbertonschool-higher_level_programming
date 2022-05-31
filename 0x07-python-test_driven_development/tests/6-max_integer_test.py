4d762deb05c70ddbe6ee6a82410f59c5ccfe082a
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-05-30 23:07:03 -0500

    Adding test 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,6 +4,7 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
     """
     Unittest for max_integer([..])
@@ -16,11 +17,10 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([-1, -2, -30, -6]), -1)
         self.assertEqual(max_integer([[1, 2], [1, 2, 3], [1]]), [1, 2, 3])
         self.assertEqual(max_integer([]), None)
-    
+
     def faild_test(self):
         self.assertRaises(TypeError, max_integer, (["hola", [1, 2, 3], [1]]))
         self.assertRaises(TypeError, max_integer, (["h", 2, 1]))
         self.assertRaises(TypeError, max_integer, None)
         self.assertRaises(TypeError, max_integer, (["a", 3.2, 5]))
         self.assertRaises(TypeError, max_integer, (["a", [1, 2], 5]))
-        
