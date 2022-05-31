d1026957bcb367f4c76b3489a59be2d6970eaa13
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-05-31 18:42:02 -0500

    Adding task 4

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -20,8 +20,8 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return string
         for i in range(self.__height):
-            string += "#" * self.__width + " \n"
-            if i == self.__height - 1:
+            string += "#" * self.__width
+            if i < self.__height - 1:
                 string += "\n"
         return string
 
