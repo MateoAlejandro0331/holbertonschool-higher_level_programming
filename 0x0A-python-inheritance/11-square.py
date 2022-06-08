b406cc1f07857b2e77b0a0590fc21180d97bdff4
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-06-07 23:40:42 -0500

    Adding task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+"""Import the class"""
+Rectangle = __import__('9-rectangle').Rectangle
+"""
+    Class Rectangle that inherits from BaseGeometry
+"""
+
+
+class Square(Rectangle):
+    """
+        My class
+    """
+
+    def __init__(self, size):
+        """Constructor method"""
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return f"[Rectangle] {self.__size}/{self.__size}"
