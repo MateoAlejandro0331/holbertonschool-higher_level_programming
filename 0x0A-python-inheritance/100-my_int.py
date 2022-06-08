dad3748a0a282b1c6f8e20ae26fcc01ddbbb983f
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-06-07 23:57:58 -0500

    hdksj

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    Import the class
+    
+"""
+
+class MyInt(int):
+    """ My class"""
+
+    def __eq__(self, num):
+        return int.__ne__(self, num)
+
+    def __ne__(self, num):
+        return int.__eq__(self, num)
