973af086443069f71e06a7f8001d72891986337a
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-06-08 16:10:48 -0500

    Adding task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    Funtion to write and create a file
+"""
+
+
+def write_file(filename="", text=""):
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write(text)
+        return len(text)
