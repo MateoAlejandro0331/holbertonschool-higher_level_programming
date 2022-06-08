2523f03130c4e69f1a910bfbc0c992dd7424bd14
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-06-07 22:38:39 -0500

    Adding task 7

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -10,6 +10,7 @@ class BaseGeometry:
     """
 
     def area(self):
+        """Method not defined"""
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
