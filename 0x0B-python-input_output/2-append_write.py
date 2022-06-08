266fd34ada77b99e8177e1a0cb9a6406dc3b0934
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-06-08 16:17:55 -0500

    Adding a task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+    Funtion to read a file
+"""
+
+
+def append_write(filename="", text=""):
+    with open(filename, 'a', encoding='utf-8') as file:
+        file.write(text)
+        return len(text)
