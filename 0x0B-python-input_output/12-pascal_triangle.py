c358397013b79fd543a6e11528cde4e3eeaf22b4
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-06-09 11:54:59 -0500

    Adding task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    Function that returns a list of lists of integers
+    representing the Pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    mylist = []
+    rows = []
+    for i in range(n):
+        rows = [1]
+        if i > 0:
+            for j in range(i):
+                rows.append(sum(mylist[-1][j:j+2]))
+        mylist.append(rows)
+    return mylist
