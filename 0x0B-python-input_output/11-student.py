44b66ecef10746141439d82e44859a46222d13ec
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-06-08 22:34:15 -0500

    Adding task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+    My class
+"""
+
+
+class Student:
+    """Constructor Method"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        mydict = {}
+        if attrs is None:
+            return vars(self)
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                mydict.setdefault(key, value)
+        return mydict
+
+    def reload_from_json(self, json):
+        for key, value in json.items():
+            setattr(self, key, value)
