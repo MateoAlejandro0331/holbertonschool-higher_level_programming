cce65cf51424a52f76d12e7965613c147951d391
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-06-08 21:56:36 -0500

    adding task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+    My class
+"""
+
+
+class Student:
+    """Constructor Method"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        mydict = dict()
+        if attrs:
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    mydict.setdefault(key, value)
+            return mydict
+        return vars(self)
