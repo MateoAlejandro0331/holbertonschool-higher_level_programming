6e969b2761b1ee2a57e43e15129dda7f823b0f77
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-08-15 22:26:56 -0500

    adding task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 
 process.argv.shift();
 process.argv.shift();
-const len = process.argv.length - 2;
+const len = process.argv.length;
 /* for (let i = 0; i < len - 2; i++) {
   process.argv[i] = parseInt(process.argv[i]);
 } */
@@ -11,5 +11,5 @@ if (len < 0 || len === 1) {
 }
 else {
   process.argv.sort((a, b) => a - b);
-  console.log(process.argv[len]);
+  console.log(process.argv[len - 2]);
 }
