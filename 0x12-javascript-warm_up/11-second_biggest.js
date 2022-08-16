6c908d740fa5165c8641953ae92c3d160693bcfd
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-08-15 22:27:30 -0500

    adding task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -7,7 +7,7 @@ const len = process.argv.length;
   process.argv[i] = parseInt(process.argv[i]);
 } */
 if (len < 0 || len === 1) {
-  console.log('1');
+  console.log(0);
 }
 else {
   process.argv.sort((a, b) => a - b);
