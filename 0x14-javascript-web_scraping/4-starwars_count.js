70a48d3edda874ed7c50a3222e417288d82d9d40
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-08-17 19:00:52 -0500

    adding task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const axios = require('axios');
+const url = 'https://swapi-api.hbtn.io/api/films/';
+
+axios.get(url).then(function (response) {
+  // console.log(response.data.results[1].characters);
+  for (const string of response.data.results[0].characters) {
+    if (string.search('18') > 0) {
+      axios.get(string).then(function (response1) {
+        console.log(response1.data.films.length);
+      });
+      break;
+    }
+  }
+});
