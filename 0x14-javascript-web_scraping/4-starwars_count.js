834858806c69d93ed9098a88eaa09c605bff053b
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-08-17 21:22:57 -0500

    adding task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,16 @@
 #!/usr/bin/node
 
 const axios = require('axios');
-const url = process.argv[2];
+const ApiURL = process.argv[2];
 
-axios.get(url).then(function (response) {
-  // console.log(response.data.results[0].characters);
-  for (const string of response.data.results[0].characters) {
-    if (string.search('18') > 0) {
-      axios.get(string).then(function (response1) {
-        console.log(response1.data.films.length);
-      });
-      break;
+axios.get(ApiURL).then((response) => {
+  let count = 0;
+  for (const result of response.data.results) {
+    for (const character of result.characters) {
+      if (character.includes('18')) {
+        count++;
+      }
     }
   }
-})
-  .catch(function (error) {
-    if (error.response) {
-      console.log('code:', error.response.status);
-    }
-  });
+  console.log(count);
+});
