6ad6cff9d68e4ee36d543443a03f726359b04cc4
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-08-30 18:01:59 -0500

    adding task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""script that takes in an argument and displays
+    all values in the states table of hbtn_0e_0_usa
+    where name matches the argument."""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                                    passwd=argv[2], db=argv[3])
+
+    if argv[4] == 'Arizona':
+        mycursor = database.cursor()
+        mycursor.execute("SELECT * FROM states WHERE name LIKE BINARY  \
+                            '{}' ORDER BY id ASC".format(argv[4]))
+        result = mycursor.fetchall()
+        for value in result:
+            print(value)
