30b3d51386615710a60e363c83267e4f3fb2c078
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-08-30 16:15:39 -0500

    adding task 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""ists all states with a name starting with N (upper N)
+  https://www.geeksforgeeks.org/sql-query-to-find-the-name-of-
+  a-person-whose-name-starts-with-specific-letter/"""
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                                    passwd=argv[2], db=argv[3])
+
+    mycursor = database.cursor()
+    mycursor.execute("SELECT * FROM states WHERE NAME LIKE 'N%' ORDER BY id;")
+    result = mycursor.fetchall()
+    for value in result:
+        print(value)
