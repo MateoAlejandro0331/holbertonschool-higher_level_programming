827f245ec49dc3808ab7010b0f0fdb58ac07eea0
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-08-30 16:24:32 -0500

    adding task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """ists all states with a name starting with N (upper N)
-  https://www.geeksforgeeks.org/sql-query-to-find-the-name-of-
-  a-person-whose-name-starts-with-specific-letter/"""
+    https://www.geeksforgeeks.org/sql-query-to-find-the-name-of-
+    a-person-whose-name-starts-with-specific-letter/"""
 import MySQLdb
 from sys import argv
 
