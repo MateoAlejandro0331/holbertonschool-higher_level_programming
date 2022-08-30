b19934927d140701287b134981ee57dc74878eec
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-08-30 16:51:23 -0500

    adding task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,8 +6,6 @@
 import MySQLdb
 from sys import argv
 
-import sqlalchemy
-
 if __name__ == "__main__":
     database = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
                                     passwd=argv[2], db=argv[3])
