d53b3b24605928ba5b1207f5e34ffdb4d312e5b6
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-08-30 15:58:01 -0500

    adding task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    database = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                                    passwd=argv[2], db=argv[3])
+
+    mycursor = database.cursor()
+    mycursor.execute("SELECT * FROM states ORDER BY id;")
+    result = mycursor.fetchall()
+    for value in result:
+        print(value)
