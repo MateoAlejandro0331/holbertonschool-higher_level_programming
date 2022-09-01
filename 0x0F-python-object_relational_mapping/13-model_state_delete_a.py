7df150bc439f75581d31d0f3ed5e8f2413f4e7e4
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-09-01 14:56:55 -0500

    adding task 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script that changes the name of a
+    State object from the database
+    hbtn_0e_6_usa, ojala pase
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    database = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    sesion = Session(database)
+    query = sesion.query(State).order_by(State.id)
+    for instance in query:
+        if 'a' in instance.name:
+            sesion.delete(instance)
+            sesion.commit()
