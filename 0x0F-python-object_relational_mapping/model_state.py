5ff6f2490a86dc76e9466a4e06ac68607603a87a
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-09-01 10:27:40 -0500

    changing import order

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """Define class State
 """
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
+
 
 class State(Base):
     """Starting the class"""
     __tablename__ = 'states'
-
     id = Column(Integer(), autoincrement=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
