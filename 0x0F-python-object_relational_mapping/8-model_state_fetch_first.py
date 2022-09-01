53913a068907433313471923f306decf258e9c4a
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-09-01 10:33:54 -0500

    adding task 8

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Start link class to table in database 
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import Session
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    database = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    sesion = Session(database)
+    """Query to the database"""
+    query = sesion.query(State).filter(State.id == 1)
+    for instance in query:
+        print(f"{instance.id}: {instance.name}")
