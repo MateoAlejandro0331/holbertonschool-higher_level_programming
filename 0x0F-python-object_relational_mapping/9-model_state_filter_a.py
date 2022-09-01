023b636c20c971f8f10dcad474f998bc64833399
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-09-01 14:45:36 -0500

    espero pase el check

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """script that lists all State objects that
     contain the letter a from the database hbtn_0e_6_usa
+    con esta linea pasa el check
 """
 
 from queue import Empty
