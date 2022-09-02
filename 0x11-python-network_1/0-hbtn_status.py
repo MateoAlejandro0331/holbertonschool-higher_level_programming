65d782b31ff6e79da0ab68a865f5bb23e7983f7c
MateoAlejandro0331 <mateo.alejandro0331@gmail.com>
2022-09-02 16:50:16 -0500

    adding task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Python script that fetches https://intranet.hbtn.io/status"""
 
-import urllib.request
+from urllib import request
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    with request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.read()
         print("Body response:")
         print(f"\t- type: {type(content)}")
